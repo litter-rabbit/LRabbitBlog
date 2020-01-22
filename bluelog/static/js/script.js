cf81c0358fe143497c9a831264adf06d5f6bae6a
litter-rabbit <709343607@qq.com>
2020-01-22 13:22:33 +0800

    about 更新

--- bluelog/static/js/script.js
+++ bluelog/static/js/script.js
@@ -5,6 +5,23 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip(
         {title: render_time}
     );
+
+
+    var OriginTitile = document.title;
+	var titleTime;
+	document.addEventListener('visibilitychange', function() {
+		if (document.hidden) {
+			document.title = '(つェ⊂)看不到我~ ' + OriginTitile;
+			clearTimeout(titleTime);
+		}
+		else {
+			document.title = '(*´∇｀*) 又在想我啦~ ' + OriginTitile;
+			titleTime = setTimeout(function() {
+				document.title = OriginTitile;
+			}, 2000);
+		}
+});
+
 });
 
 //音乐播放器
@@ -49,4 +66,9 @@ biu_toclick.addEventListener('click',function () {
 		biu_click.firstChild.textContent=''
 
 })
+
+//网页标签
+
+
+
 
